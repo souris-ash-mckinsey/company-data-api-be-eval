592f99a174cd99f9ddb24bb3e14f02c0dd6d4376
Souris Ash <souris_ash@mckinsey.com>
2023-02-03 17:13:04 +0530

    feat: add validation

--- src/routes/companyDataRoute.js
+++ src/routes/companyDataRoute.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const companyDataRoute = express.Router();
 const { saveCompanyDataController } = require('../controllers/companyDataController');
+const { requestSchemaValidator } = require('../utils/middleware/requestSchemaValidator');
+const { saveRequestSchema } = require('../utils/requestSchemas');
+
+companyDataRoute.use('/save', requestSchemaValidator({
+  'POST': [
+    {
+      type: 'body',
+      schema: saveRequestSchema
+    }
+  ]
+}));
 
 companyDataRoute.post('/save', saveCompanyDataController);
 
